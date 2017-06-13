c64de4aa5fc907621b3fed1490064b6d37de772f
AbrhmSanchez <abrhmsanchez@gmail.com>
2017-06-13 11:16:18 -0400

    - Utilizing __slots__ for efficiency? - Added __eq__ method to the song class - Added methods to the songList class

--- Songsched/songsched.py
+++ Songsched/songsched.py
@@ -1,11 +1,12 @@
 import datetime
-from operator import itemgetter
+from operator import attrgetter
 
 today = datetime.date.today()
 defaultDate = datetime.date(today.year, 1, 1)
 
 class song(object):
     """Store song name, number of times sung and last time sung."""
+    __slots__ = ['name', 'reps', 'date']
     def __init__(self, name, reps=0, date=defaultDate):
         self.name = name
         self.reps = reps
@@ -14,6 +15,12 @@ class song(object):
     def __str__(self):
         s = '{0:35} | {1:3} | {2:%d/%m/%Y}'
         return s.format(self.name, self.reps, self.date)
+
+    def __eq__(self, other):
+        nameCheck = self.name == other.name
+        repsCheck = self.reps == other.reps
+        dateCheck = self.date == other.date
+        return nameCheck and repsCheck and dateCheck
 
     def to_csv(self):
         """Return a string in csv format."""
@@ -36,8 +43,13 @@ class song(object):
 
 class songList(object):
     """Store a list of songs and some useful methods."""
+    __slots__ = ['songList', 'rn', 'rr', 'rd']
     def __init__(self, songList=[]):
+        if not all(isinstance(s, song) for s in songList):
+            raise TypeError('All items must be songs!')
         self.songList = songList
+        # Variables used to reverse the sorting
+        self.rn = self.rr = self.rd = False
 
     @classmethod
     def from_file(cls, file):
@@ -64,3 +76,27 @@ class songList(object):
         with open(file+'.csv', 'w') as f:
             str_list = [song.to_csv() for song in self.songList]
             f.write('\n'.join(str_list))
+
+    def add_song(self, s):
+        """Adds a song to the song list."""
+        if not isinstance(s, song):
+            raise TypeError('That is not a song!')
+        self.songList.append(s)
+
+    def by_name(self):
+        self.songList.sort(key=attrgetter('name'), reverse = self.rn)
+        self.rn = not self.rn
+        self.rr = False
+        self.rd = False
+
+    def by_reps(self):
+        self.songList.sort(key=attrgetter('reps'), reverse = self.rr)
+        self.rn = False
+        self.rr = not self.rr
+        self.rd = False
+
+    def by_date(self):
+        self.songList.sort(key=attrgetter('date'), reverse = self.rd)
+        self.rn = False
+        self.rr = False
+        self.rd = not self.rd

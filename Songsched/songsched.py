39c075734de26e3d2558538b439145fbe1177509
AbrhmSanchez <abrhmsanchez@gmail.com>
2017-06-13 08:44:17 -0400

    Changed file extension to csv, made required changes to the classes

--- Songsched/songsched.py
+++ Songsched/songsched.py
@@ -15,6 +15,10 @@ class song(object):
         s = '{0:35} | {1:3} | {2:%d/%m/%Y}'
         return s.format(self.name, self.reps, self.date)
 
+    def to_csv(self):
+        """Return a string in csv format."""
+        return '{0},{1},{2:%d/%m/%Y}'.format(self.name, self.reps, self.date)
+
     @classmethod
     def from_str(cls, s):
         """Return a song object from the string representation."""
@@ -23,33 +27,40 @@ class song(object):
         date = datetime.date(int(s[50:54]), int(s[47:49]), int(s[44:46]))
         return cls(name, reps, date)
 
+    @classmethod
+    def from_csv(cls, s):
+        """Return a song object from a csv file line."""
+        name, reps, s = s.split(',')
+        date = datetime.date(int(s[6:10]), int(s[3:5]), int(s[:2]))
+        return cls(name, int(reps), date)
+
 class songList(object):
-    """Store list of songs and useful methods."""
+    """Store a list of songs and some useful methods."""
     def __init__(self, songList=[]):
         self.songList = songList
 
     @classmethod
     def from_file(cls, file):
-        """Return songList object from a file."""
+        """Return songList object from a csv file."""
         try:
-            with open(file+'.txt') as f:
-                songList = [song.from_str(line) for line in f]
+            with open(file+'.csv') as f:
+                songList = [song.from_csv(line) for line in f]
                 return cls(songList)
         except Exception as e:
             print('There was an exception:', e)
-            return cls([])
+            return cls()
 
     def to_file(self, file):
-        """Writes songList to file using the song string representation.
+        """Writes songList to file using the song.to_csv() method.
         
-        The method backs up the file if it already exists.
+        This method backs up the file if it already exists.
         """
         try:
-            with open(file+'.txt') as f:
+            with open(file+'.csv') as f:
                 with open(file+'.BAK', 'w') as b:
                     b.write(f.read())
         except:
             pass
-        with open(file+'.txt', 'w') as f:
-            str_list = [str(song) for song in self.songList]
+        with open(file+'.csv', 'w') as f:
+            str_list = [song.to_csv() for song in self.songList]
             f.write('\n'.join(str_list))
